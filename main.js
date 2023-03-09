a784aa2f611dc7a89515a6626dbec1a608411948
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-03-09 13:44:03 -0700

    Full heart

--- main.js
+++ main.js
@@ -3,8 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const like_glyphs = document.getElementsByClassName('like-glyph');
 
-
+for (let i = 0; i < like_glyphs.length; i++) {
+  like_glyphs[i].addEventListener("click", (event) => {
+    event.preventDefault();
+    mimicServerCall()
+    .then((response) => {
+      console.log(event.target)
+      if(event.target.innerHTML == FULL_HEART) {
+        event.target.innerHTML = EMPTY_HEART
+        event.target.className = ""
+      }
+      else {
+        event.target.innerHTML = FULL_HEART
+        event.target.className = "activated-heart"
+      }
+    })
+    .catch(err => {
+      document.getElementById("modal").className = ""
+      setTimeout(() => {
+        document.getElementById("modal").className = "hidden"
+      }, 3000);
+    })
+  });
+}
 
 
 //------------------------------------------------------------------------------
